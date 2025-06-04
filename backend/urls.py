e0e832d99d04af8352c33c6246064071fa58e9d4
mood350 <princiuso350@gmail.com>
2025-06-04 16:15:34 +0000

    Ajout des urls

--- backend/urls.py
+++ backend/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('conditions_utilisation/', views.conditions_utilisation, name='conditions_utilisation'),
     path('support_contact/', views.support_contact, name='support_contact'),
 
-    path('achat/', views.achat, name='achat'),
-    path('vente/', views.vente, name='vente'),
+    path('achat/<int:crypto_id>/', views.achat, name='achat'),
+    path('vente/<int:crypto_id>/', views.vente, name='vente'),
     path('profiles/', views.profiles, name='profiles')
    
     
